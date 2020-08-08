e41dc1b94f131262759de6771b2faa30f9cdd905
burn-c <chopcsom@gmail.com>
2020-08-07 22:20:30 -0300

    Feat: Classes and Connections controllers

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
+
 const app = express();
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use(routes);
-app.listen(3333)
+app.listen(3333);
+
+console.info('    🔥      Server start')
